796a8766d3c6ac1f08b445507db742b7a82dc4bc
Lluc <lluc.vilap@gmail.com>
2026-06-14 16:01:20 +0200

    feat: create tables

--- Game_of_life_data/sql/crate_tables.sql
+++ Game_of_life_data/sql/crate_tables.sql
@@ -1,4 +1,15 @@
+CREATE TABLE runs (
+    run_id SERIAL NOT NULL PRIMARY KEY,
+    execution_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    rows INT,
+    cols INT,
+    generations INT,
+    topology_mode VARCHAR(50),
+    description TEXT
+);
+
 CREATE TABLE life (
+    run_id INT NOT NULL REFERENCES runs(run_id),
     generation INT,
     row_num INT,
     col_num INT,
